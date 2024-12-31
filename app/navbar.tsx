6b6b2e7d3726802d4e245fe1b7eecfe06184f80b
pavansarvesh <pavansarvesh31@gmail.com>
2024-12-31 23:14:19 +0530

    1st prototype(changes)

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex justify-end text-xl font-bold'>
-      <input type='button' value='Projects' />
+      <input type='button' value='Projects' className='hover:text-yellow-500' />
       <input
         type='button'
         value='Resume'
-        className='m-10 ml-28 mr-72 bg-yellow-500 text-gray-950 p-3 rounded-md'
+        className='m-10 ml-28 mr-72 bg-yellow-500 text-gray-950 p-3 rounded-md hover:text-yellow-500 hover:bg-neutral-950'
       />
     </div>
   );
