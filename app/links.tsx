b977e1944d9e6d3ae87b09b695a019008595f489
pavansarvesh <pavansarvesh31@gmail.com>
2025-01-13 20:25:26 +0530

    added redirect links

--- app/links.tsx
+++ app/links.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import Image from 'next/image';
-import Linkedin from '../public/linkedin.svg';
-import Github from '../public/github.svg';
-import Twitter from '../public/twitter.svg';
+import LinkedinIcon from '../public/linkedin.svg';
+import GithubIcon from '../public/github.svg';
+import TwitterIcon from '../public/twitter.svg';
 
 const Links = () => {
   return (
     <div className='flex justify-between grow'>
-      <Image
-        src={Linkedin}
-        height={50}
-        width={50}
-        alt='linkedin'
-        className='mr-6'
-      />
-      <Image
-        src={Github}
-        height={50}
-        width={50}
-        alt='github'
-        // onClick='https://www.linkedin.com/in/r-pavan-sarvesh-462798256/'
-        className='mr-6'
-      />
-      <Image
-        src={Twitter}
-        height={50}
-        width={50}
-        alt='twitter'
-        className='size-10 mt-1'
-      />
+      <a
+        href='https://www.linkedin.com/in/r-pavan-sarvesh-462798256/'
+        target='_blank'
+      >
+        <Image
+          src={LinkedinIcon}
+          height={50}
+          width={50}
+          alt='linkedin'
+          className='mr-6'
+        />
+      </a>
+
+      <a href='https://github.com/pavansarvesh' target='_blank'>
+        <Image
+          src={GithubIcon}
+          height={50}
+          width={50}
+          alt='github'
+          className='mr-6'
+        />
+      </a>
+      <a href='https://x.com/pavansarveshr' target='_blank'>
+        <Image
+          src={TwitterIcon}
+          height={50}
+          width={50}
+          alt='twitter'
+          className='size-10 mt-1'
+        />
+      </a>
     </div>
   );
 };
