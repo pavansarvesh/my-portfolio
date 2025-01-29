60d47246da1feccfe83c1e7ae079e2edb0454a01
pavansarvesh <pavansarvesh31@gmail.com>
2025-01-29 15:06:32 +0530

    draft5

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,28 +1,21 @@
-import { allPosts, MDX} from 'contentlayer/generated';
+import { allPosts } from 'contentlayer/generated';
 import { notFound } from 'next/navigation';
-import { useMDXComponent } from 'next-contentlayer2/hooks';
-import type { MDXComponents } from 'mdx/types';
 import GoBack from '@/app/blog/components/GoBack';
 import Mdx from '@/app/blog/components/mdx-components';
 
-// import custom components and add below so that you can use it in mdx files
-const mdxComponents: MDXComponents = {};
-
-interface PageProps{
-  params: { slug: string };
+interface Props {
+  params: Promise<{ slug: string }>;
 }
 
 async function getBlogsFromParams(slug: string) {
   const post = allPosts.find((post) => post._raw.flattenedPath === slug);
   if (!post) notFound();
   return post;
-
 }
 
-const PostLayout = async ({params}: PageProps) => {
-  const { slug } = await params;
-  const post = await getBlogsFromParams(slug)
-
+export default async function PostLayout({ params }: Props) {
+  const resolvedParams = await params;
+  const post = await getBlogsFromParams(resolvedParams.slug);
 
   return (
     <article className='prose prose-lg prose-neutral prose-headings:text-slate-200 mx-auto mt-5 text-yellow-500 prose-p:text-yellow-500 prose-code:text-lg flex flex-col flex-wrap'>
@@ -33,23 +26,20 @@ const PostLayout = async ({params}: PageProps) => {
       <GoBack />
     </article>
   );
-};
+}
 
-export default PostLayout;
+export async function generateMetadata({ params }: Props) {
+  const resolvedParams = await params;
+  const post = await getBlogsFromParams(resolvedParams.slug);
 
-export const generateStaticParams = async () => {
-  return allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
-};
+  return {
+    title: post.title,
+    description: post.description,
+  };
+}
 
-export const generateMetadata = async ({
-  params,
-}: {
-  params: { slug: string };
-}) => {
-  const { slug } = await params;
-  const post = allPosts.find((post) => post._raw.flattenedPath === slug);
-
-  if (!post) notFound();
-
-  return { title: post.title };
-};
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post._raw.flattenedPath,
+  }));
+}
